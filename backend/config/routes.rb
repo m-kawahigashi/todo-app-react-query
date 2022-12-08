c3fe52172a19a798f40fcc82c91988829ea55b6e
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-12-08 15:56:30 +0900

    add: 更新用ルーティングを追加 #15

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :test, only: %i[index]
-      resources :todos, only: [:index, :create, :destroy]
+      resources :todos, only: [:index, :create, :update, :destroy]
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: "api/v1/auth/registrations"
