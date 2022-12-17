feb78f468c2cdb9caeb594255a0b24c284630d5d
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-12-17 19:07:43 +0900

    update: redux化反映（途中）#16

--- frontend/app/src/components/pages/todo/hooks/useGetTodo.ts
+++ frontend/app/src/components/pages/todo/hooks/useGetTodo.ts
@@ -1,10 +1,18 @@
 import { useCallback, useState } from "react"
 import { getTodos } from "lib/api/todos"
 import { Todo } from "interfaces/index"
+import { useSelector } from "react-redux"
+import { RootState } from "redux/store/store"
+import { useDispatch } from "react-redux"
+import { todoActionTypes } from "redux/actions/ActionTypes"
+import { getTodo } from "redux/actions/TodoActions"
 
 export const useGetTodo = () => {
     const [ todos, setTodos ] = useState<Todo[]>([])
     const [ isError, setIsError ] = useState<boolean>(false)
+
+    const selector = useSelector((state: RootState) => state.todos)
+    const dispatch = useDispatch()
 
     const handleGetTodos = useCallback(async () => {
         try {
@@ -12,7 +20,12 @@ export const useGetTodo = () => {
                 console.log(res)
 
                 setTodos(res.data.todos)
+                // dispatch({ type: getTodo(res.data.todos) }) // 無限ループ発生
+                // dispatch(getTodo(res.data.todos))　// 無限ループ発生
+
                 console.log(res.data.message)
+                // console.log(res.data.todos)
+                // console.log(selector)
             } catch (err) {
                 setIsError(true)
                 console.log(err)
--- frontend/app/src/components/pages/todo/pages/Home.tsx
+++ frontend/app/src/components/pages/todo/pages/Home.tsx
@@ -4,14 +4,32 @@ import { TodoForm } from "./TodoForm"
 
 import { AuthContext } from "App"
 import { useGetTodo } from "../hooks/useGetTodo"
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+// import { StoreState } from "redux/types/todo/types"
+import { RootState } from "redux/store/store"
+import { getTodo } from "redux/actions/TodoActions"
+
+
+//RootState
 
 const Home: FC = memo(() => {
   const { isSignedIn, currentUser } = useContext(AuthContext)
   const { handleGetTodos, isError, todos, setTodos } = useGetTodo()
 
+  const selector = useSelector( (state: RootState) => state.todos )
+  const dispatch = useDispatch()
+
+  // console.log(selector)
+  // console.log(dispatch)
+
   useEffect(() => {
     handleGetTodos()
+    // dispatch(getTodo(todos))　// 無限ループ発生
   }, [ handleGetTodos ])
+
+  // dispatch(getTodo(todos))　// 無限ループ発生
+  console.log(selector)
 
   if (!isSignedIn || !currentUser) {
     return <h1>ログインできてないよー</h1>
