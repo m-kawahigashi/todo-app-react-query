3c4f9d87cca5db23a88309fd33dda22ff6d1a4c8
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-12-22 12:18:52 +0900

    update: redux-thunkとredux devToolsの使用設定を追加 #16

--- frontend/app/src/redux/store/store.tsx
+++ frontend/app/src/redux/store/store.tsx
@@ -1,5 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import { todoReducer } from "redux/reducers/todoReducer";
+import thunk from "redux-thunk";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const RootReducer = combineReducers({
     todos: todoReducer,
@@ -7,5 +16,5 @@ const RootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof RootReducer>
 
-const store = createStore(RootReducer)
+const store = createStore(RootReducer, composeEnhancers(applyMiddleware(thunk)))
 export default store
