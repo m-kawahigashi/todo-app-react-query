68daf01d840234dbe418a0acf6bae4864f3bbeb6
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2023-01-12 23:07:16 +0900

    add: react-query用のProviderとdevtoolsの設定を追加

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -6,13 +6,29 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from 'redux/store/store';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const container = document.getElementById('root');
+const queryClient = new QueryClient();
+
+// if ( container ) {
+//   const root = createRoot(container);
+//   root.render(
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     );
+// }
 
 if ( container ) {
   const root = createRoot(container);
   root.render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Provider>
+      </QueryClientProvider>
     );
 }
