d9131eff3f43c59cc487a27ee9dcc1697bac307a
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2023-01-13 12:26:09 +0900

    remove: reduxのProviderを削除

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -3,32 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux'
-import store from 'redux/store/store';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const container = document.getElementById('root');
 const queryClient = new QueryClient();
 
-// if ( container ) {
-//   const root = createRoot(container);
-//   root.render(
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//     );
-// }
-
 if ( container ) {
   const root = createRoot(container);
   root.render(
       <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <App />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </Provider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     );
 }
